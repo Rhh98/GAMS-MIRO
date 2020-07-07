6b38278a7943e8cc426c5856ec0d529558e22611
Michael Ferris <ferris@cs.wisc.edu>
2020-07-07 10:03:53 -0500

    deleted temporary 225 dirs

--- HarryPotter/225b/gamsnext.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-gmsgennt.cmd "C:\Users\hasee\DOCUME~1\GitHub\GAMS-M~1\HARRYP~1\225b\" "C:\Users\hasee\Documents\GitHub\GAMS-MIRO\HarryPotter\" %1 "C:\Users\hasee\Documents\GitHub\GAMS-MIRO\HarryPotter\225a\empcntr.dat" "E:\31.1\" "CONOPT"
--- HarryPotter/225c/gamsnext.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-gmsgennt.cmd "C:\Users\hasee\Documents\GitHub\GAMS-MIRO\HarryPotter\225c\" "C:\Users\hasee\Documents\GitHub\GAMS-MIRO\HarryPotter\" %1 "C:\Users\hasee\Documents\GitHub\GAMS-MIRO\HarryPotter\225c\gamscntr.dat" "E:\31.1\" "CONOPT"
