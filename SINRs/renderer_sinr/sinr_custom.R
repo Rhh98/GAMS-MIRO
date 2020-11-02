077fa2c7b4646fb55201df1642770b20ad5324ec
Frederik Proske <fproske@gams.com>
2020-11-02 10:30:15 +0100

    Namespace custom renderer of sinr model

--- SINRs/renderer_sinr/sinr_custom.R
+++ SINRs/renderer_sinr/sinr_custom.R
@@ -7,19 +7,19 @@ sinrOutput <- function(id, height = NULL, options = NULL, path = NULL){
   } 
   tagList( 
     #define rendererOutput function here 
-    plotlyOutput(ns('bar'),height=height)
+    plotly::plotlyOutput(ns('bar'),height=height)
     
   ) 
 }
 
 renderSinr <- function(input, output, session, data, options = NULL, path = NULL, ...){ 
   #renderer 
-  output$bar<-renderPlotly({
+  output$bar<-plotly::renderPlotly({
     # barplot(rbind(data$maxworst,data$maxaverage),main="SINR of Max_Worst and Max_Average model" , 
     #         names.arg = data$n,legend=c("Worst","Average"),xlab = "User",ylab = "SINR",col=c("blue","green"),beside=TRUE)
-  pl<-plot_ly(x=data$n,y=data$maxworst,name="Worst",type='bar')
-  pl<-add_trace(pl,y=data$maxaverage,name="Average")
-  pl<-layout(pl,title="SINR of Max_Worst and Max_Average model",
+  pl<-plotly::plot_ly(x=data$n,y=data$maxworst,name="Worst",type='bar')
+  pl<-plotly::add_trace(pl,y=data$maxaverage,name="Average")
+  pl<-plotly::layout(pl,title="SINR of Max_Worst and Max_Average model",
              yaxis=list(title="SINR"),
              xaxis=list(title="User"))
   })
@@ -34,20 +34,20 @@ pieOutput <- function(id, height = NULL, options = NULL, path = NULL){
   } 
   tagList( 
     #define rendererOutput function here 
-    fluidRow(column(6,plotlyOutput(ns("pie1"),height=height)),
-             column(6,plotlyOutput(ns("pie2"),height=height)))
+    fluidRow(column(6,plotly::plotlyOutput(ns("pie1"),height=height)),
+             column(6,plotly::plotlyOutput(ns("pie2"),height=height)))
   )
 }
 
 renderPie <- function(input, output, session, data, options = NULL, path = NULL, ...){ 
   #renderer 
-  output$pie1<-renderPlotly({
-    pl<-plot_ly(labels=data$n,values=round(data$maxworst,3),type = 'pie')
-    pl<-layout(pl,title="Power of Max_Worst Model")
+  output$pie1<-plotly::renderPlotly({
+    pl<-plotly::plot_ly(labels=data$n,values=round(data$maxworst,3),type = 'pie')
+    pl<-plotly::layout(pl,title="Power of Max_Worst Model")
     
   })
-  output$pie2<-renderPlotly({
-    pl<-plot_ly(labels=data$n,values=round(data$maxaverage,3),type = 'pie')
-    pl<-layout(pl,title="Power of Max_Average Model")
+  output$pie2<-plotly::renderPlotly({
+    pl<-plotly::plot_ly(labels=data$n,values=round(data$maxaverage,3),type = 'pie')
+    pl<-plotly::layout(pl,title="Power of Max_Average Model")
   })
 }
